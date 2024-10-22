892b4ed672e2d37f25d844062a548f3c9263f4e5
Danila Smirnov <ylptred@gmail.com>
2024-10-22 13:52:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,91 +3,117 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+#include <chrono>
+#include <omp.h>
 
 void printMatrix(const std::vector<std::vector<double> >& matrix) {
-    for (const auto& row : matrix) {
-        for (double elem : row) {
-            std::cout << std::setw(10) << std::setprecision(4) << elem << " ";
-        }
-        std::cout << std::endl;
-    }
+   for (const auto& row : matrix) {
+       for (double elem : row) {
+std::cout << std::setw(10) << std::setprecision(4) << elem << " ";
+       }
+       std::cout << std::endl;
+   }
 }
 
-bool invertMatrix(std::vector<std::vector<double>>& A, std::vector<std::vector<double>>& inverse) {
-    int n = A.size();
-    
-    // Инициализируем единичную матрицу
-    inverse.assign(n, std::vector<double>(n, 0));
-    for (int i = 0; i < n; ++i) {
-        inverse[i][i] = 1.0;
-    }
 
-    // Прямой ход
-    for (int i = 0; i < n; ++i) {
-        // Находим максимальный элемент для выбора главного элемента
-        double maxElem = std::abs(A[i][i]);
-        int maxRow = i;
-        
-        for (int k = i + 1; k < n; ++k) {
-            if (std::abs(A[k][i]) > maxElem) {
-                maxElem = std::abs(A[k][i]);
-                maxRow = k;
-            }
-        }
+bool invertMatrix(std::vector<std::vector<double> >& A, std::vector<std::vector<double> >& inverse) {
+   int n = A.size();
+  
+   // Инициализируем единичную матрицу
+   inverse.assign(n, std::vector<double>(n, 0));
+   for (int i = 0; i < n; ++i) {
+       inverse[i][i] = 1.0;
+   }
 
-        // Меняем строки местами
-        std::swap(A[i], A[maxRow]);
-        std::swap(inverse[i], inverse[maxRow]);
 
-        // Обнуляем столбец под диагональю
-        double diagElem = A[i][i];
-        if (std::abs(diagElem) < 1e-9) {
-            std::cout << "Матрица является вырожденной, обратная матрица не существует." << std::endl;
-            return false;
-        }
+   // Прямой ход
+   for (int i = 0; i < n; ++i) {
+       // Находим максимальный элемент для выбора главного элемента
+       double maxElem = std::abs(A[i][i]);
+       int maxRow = i;
+      
+       for (int k = i + 1; k < n; ++k) {
+           if (std::abs(A[k][i]) > maxElem) {
+               maxElem = std::abs(A[k][i]);
+               maxRow = k;
+           }
+       }
 
-        for (int j = 0; j < n; ++j) {
-            A[i][j] /= diagElem;
-            inverse[i][j] /= diagElem;
-        }
 
-        for (int k = 0; k < n; ++k) {
-            if (k != i) {
-                double coef = A[k][i];
-                for (int j = 0; j < n; ++j) {
-                    A[k][j] -= A[i][j] * coef;
-                    inverse[k][j] -= inverse[i][j] * coef;
-                }
-            }
-        }
-    }
+       // Меняем строки местами
+       std::swap(A[i], A[maxRow]);
+       std::swap(inverse[i], inverse[maxRow]);
 
-    return true;
+
+       // Обнуляем столбец под диагональю
+       double diagElem = A[i][i];
+       if (std::abs(diagElem) < 1e-9) {
+           std::cout << "Матрица является вырожденной, обратная матрица не существует." << std::endl;
+           return false;
+       }
+
+
+       // Параллелим деление по строкам
+       #pragma omp parallel for simd
+       for (int j = 0; j < n; ++j) {
+           A[i][j] /= diagElem;
+inverse[i][j] /= diagElem;
+       }
+
+
+       // Обнуляем остальные строки
+       #pragma omp parallel for
+       for (int k = 0; k < n; ++k) {
+           if (k != i) {
+               double coef = A[k][i];
+
+
+               // Векторизируем операции вычитания
+               #pragma omp simd
+               for (int j = 0; j < n; ++j) {
+                   A[k][j] -= A[i][j] * coef;
+                   inverse[k][j] -= inverse[i][j] * coef;
+               }
+           }
+       }
+   }
+
+
+   return true;
 }
+
 
 int main() {
-    std::srand(static_cast<unsigned>(std::time(0)));  // Инициализация генератора случайных чисел
+   std::srand(static_cast<unsigned>(std::time(0)));  // Инициализация генератора случайных чисел
 
-    int n;
-    std::cout << "Введите размерность матрицы n x n: ";
-    std::cin >> n;
 
-    // Генерация random-матрицы
-    std::vector<std::vector<double>> A(n, std::vector<double>(n));
-    std::cout << "Сгенерированная матрица A:" << std::endl;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            A[i][j] = (std::rand() % 10) + 1;  // Генерация значений от 1 до 10
-        }
-    }
-    
-    printMatrix(A);
+   int n;
+   std::cout << "Enter matrix dimension n x n: ";
+   std::cin >> n;
 
-    std::vector<std::vector<double>> A_inv;
-    if (invertMatrix(A, A_inv)) {
-        std::cout << "Обратная матрица A^(-1):" << std::endl;
-        printMatrix(A_inv);
-    }
+   auto start = std::chrono::high_resolution_clock::now();
+   // Генерация случайной матрицы
+   std::vector<std::vector<double> > A(n, std::vector<double>(n));
+   std::cout << "Generated matrix A:" << std::endl;
+   for (int i = 0; i < n; ++i) {
+       for (int j = 0; j < n; ++j) {
+           A[i][j] = (std::rand() % 10) + 1;  // Генерация значений от 1 до 10
+       }
+   }
+  
+   printMatrix(A);
 
-    return 0;
+
+   std::vector<std::vector<double> > A_inv;
+   if (invertMatrix(A, A_inv)) {
+       std::cout << "Inverse matrix A^(-1):" << std::endl;
+       printMatrix(A_inv);
+   }
+
+   auto end= std::chrono::high_resolution_clock::now();
+   auto duration= std::chrono::duration_cast<std::chrono::milliseconds>(end- start);
+   std::cout << "Time: " << duration.count() << " ms" << "; for n = " << n << std::endl;
+
+   return 0;
 }
